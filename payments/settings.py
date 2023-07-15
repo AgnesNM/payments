9f54737a36e6f01ee36d6434fafe9263e0753d55
AgnesNM <agnesndutamwaniki@gmail.com>
2023-07-15 13:37:44 +0300

    database config

--- payments/settings.py
+++ payments/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = "payments.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "payments",
+        "USER": "payments_admin",
+        "PASSWORD": "Ndiunguro23#",
+        "HOST": "localhost",
+        "PORT": "5433"
     }
 }
 
