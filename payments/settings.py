a36e2113a1064b1e78f29d42a704845d6798c7df
AgnesNM <agnesndutamwaniki@gmail.com>
2023-08-21 08:02:36 +0300

    comment out database config on setiings.py

--- payments/settings.py
+++ payments/settings.py
@@ -99,16 +99,16 @@ WSGI_APPLICATION = "payments.wsgi.application"
 #     }
 #     }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("DATABASE_NAME"),
-        "USER": os.environ.get("DATABASE_USER"),
-        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
-        "HOST": os.environ.get("DATABASE_HOST"),
-        "PORT": os.environ.get("DATABASE_PORT")
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.environ.get("DATABASE_NAME"),
+#         "USER": os.environ.get("DATABASE_USER"),
+#         "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+#         "HOST": os.environ.get("DATABASE_HOST"),
+#         "PORT": os.environ.get("DATABASE_PORT")
+#     }
+# }
 
 
 
